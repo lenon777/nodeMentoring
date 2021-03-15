6b17c97771c8c61479bcf22efd17c5659a81aa0c
mytsak <andrii_mytsak@epam.com>
2021-03-15 13:59:32 +0200

    bug fix

--- src/services/userDataService.js
+++ src/services/userDataService.js
@@ -1,23 +1,27 @@
-export const usersList = [
-  {
-    id: "1",
-    login: "bqwe@asd.com",
-    password: "123123",
-    age: 25,
-    isDeleted: false,
-  },
-  {
-    id: "2",
-    login: "aqwe@asd.com",
-    password: "qweasd",
-    age: 30,
-    isDeleted: false,
-  },
-  {
-    id: "3",
-    login: "casqwedasd@asd.com",
-    password: "123123",
-    age: 21,
-    isDeleted: false,
-  },
-];
+module.exports = {
+    usersList: (() => {
+        return [
+            {
+                id: '1',
+                login: 'bqwe@asd.com',
+                password: '123123',
+                age: 25,
+                isDeleted: false,
+            },
+            {
+                id: '2',
+                login: 'aqwe@asd.com',
+                password: 'qweasd',
+                age: 30,
+                isDeleted: false,
+            },
+            {
+                id: '3',
+                login: 'casqwedasd@asd.com',
+                password: '123123',
+                age: 21,
+                isDeleted: false,
+            },
+        ];
+    })(),
+};
