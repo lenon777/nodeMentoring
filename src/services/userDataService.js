6afa8174c44230273c4110a79cb9b34f0150adc3
mytsak <andrii_mytsak@epam.com>
2021-03-15 12:52:22 +0200

    added auto-suggest feature

--- src/services/userDataService.js
+++ src/services/userDataService.js
@@ -1,21 +1,21 @@
 export const usersList = [
   {
     id: "1",
-    login: "qwe@asd.com",
+    login: "bqwe@asd.com",
     password: "123123",
     age: 25,
     isDeleted: false,
   },
   {
     id: "2",
-    login: "qwe@asd.com",
+    login: "aqwe@asd.com",
     password: "qweasd",
     age: 30,
     isDeleted: false,
   },
   {
     id: "3",
-    login: "asdasd@asd.com",
+    login: "casqwedasd@asd.com",
     password: "123123",
     age: 21,
     isDeleted: false,
--- src/task2.1.js
+++ src/task2.1.js
@@ -15,19 +15,19 @@ app.use(express.json());
 router.get('/users/:id', (req, res) => {
     const currentUser = usersList.find((user) => user.id === req.params.id);
     if (currentUser) {
-        res.json(currentUser);
+        res.status(200).json(currentUser);
     } else {
-        res.send('User not found');
+        res.status(404).send('User not found');
     }
 });
 
 router.post('/addUser', validateSchema(schema), (req, res) => {
     const user = findUser(req.body.id);
     if (user) {
-        res.send('This User already exist');
+        res.status(404).send('This User already exist');
     } else {
         usersList.push(req.body);
-        res.send('User was added successfully');
+        res.status(200).send('User was added successfully');
     }
 });
 
@@ -35,9 +35,9 @@ router.put('/updateUser', validateSchema(schema), (req, res) => {
     const user = findUser(req.body.id);
     if (user) {
         usersList[usersList.indexOf(user)] = req.body;
-        res.send('Updated successfully');
+        res.status(200).send('Updated successfully');
     } else {
-        res.send('User not found');
+        res.status(404).send('User not found');
     }
 });
 
@@ -45,13 +45,29 @@ router.delete('/removeUser/:id', (req, res) => {
     const user = findUser(req.params.id);
     if (user) {
         usersList[usersList.indexOf(user)].isDeleted = true;
-        res.send('Deleted successfully');
+        res.status(200).send('Deleted successfully');
     } else {
-        res.send('User not found');
+        res.status(404).send('User not found');
+    }
+});
+
+router.get('/auto-suggest/user', (req, res) => {
+    const filteredUsers = usersList
+        .filter((user) => {
+            return user.login.includes(req.query.search);
+        })
+        .sort((a, b) => {
+            return a.login.localeCompare(b.login);
+        })
+        .slice(0, req.query.limit);
+    if (filteredUsers.length !== 0) {
+        res.status(200).send(filteredUsers);
+    } else {
+        res.status(404).send('Users not found');
     }
 });
 
 app.use('/', router);
 app.use((req, res) => {
-    res.send('<h1> Page not found </h1>');
+    res.status(404).send('<h1> Page not found </h1>');
 });
