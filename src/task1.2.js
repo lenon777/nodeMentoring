19c95ef1e7f800642a37825cacc43e238ce582e1
mytsak <andrii_mytsak@epam.com>
2021-03-16 12:53:43 +0200

    added userController and fixed linter errors

--- src/task1.2.js
+++ src/task1.2.js
@@ -1,18 +1,18 @@
-import { createReadStream, createWriteStream } from "fs";
-const csv = require("csvtojson");
-const csvFilePath = "./csv/nodejs-hw1-ex1.csv";
+import { createReadStream, createWriteStream } from 'fs';
+const csv = require('csvtojson');
+const csvFilePath = './csv/nodejs-hw1-ex1.csv';
 
 const readStream = createReadStream(csvFilePath);
-const writable = createWriteStream("./src/my.txt", "utf8");
+const writable = createWriteStream('./src/my.txt', 'utf8');
 
-readStream.on("error", () => {
-  console.log(err);
+readStream.on('error', (err) => {
+    console.log(err);
 });
 
-writable.on("error", () => {
-  console.log(err);
+writable.on('error', (err) => {
+    console.log(err);
 });
 readStream
-  .pipe(csv())
-  .on("error", (err) => console.log(err))
-  .pipe(writable);
+    .pipe(csv())
+    .on('error', (err) => console.log(err))
+    .pipe(writable);
