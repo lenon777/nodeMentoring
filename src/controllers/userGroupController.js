a2ac9e2d571e5535f871e45513c60165bfc9d23c
mytsak <andrii_mytsak@epam.com>
2021-04-14 20:49:40 +0300

    changed adding to UserGroup table

--- src/controllers/userGroupController.js
+++ src/controllers/userGroupController.js
@@ -1,21 +1,18 @@
 const sequelize = require('../data-access/database');
 
 export default class UserGroup {
-    constructor(usersList, groupList) {
-        this.usersList = usersList;
-        this.groupList = groupList;
+    constructor(userGroupList) {
+        this.userGroupList = userGroupList;
     }
 
     async addUserToGroup(req, res) {
         await sequelize.sync();
         await sequelize.transaction(async (t) => {
-            const user = await this.usersList.findOne({ where: { id: req.query.userId } });
-            const group = await this.groupList.findOne({ where: { id: req.query.groupId } });
-            if (user && group) {
-                await user.addGroup(group, { transaction: t });
+            try {
+                await this.userGroupList.create(req.body, { transaction: t });
                 res.status(201).json({});
-            } else {
-                res.status(404).json({});
+            } catch (err) {
+                console.log(err);
             }
         });
     }
